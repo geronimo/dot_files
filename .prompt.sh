6663e5c9d48ce079d6375e7ce2db04cb58ec8f8a
Geronimo De Abreu <geronimodeabreu@gmail.com>
2011-11-05 22:45:19 -0430

    git and rvm prompt, first version

---
+++ .prompt.sh
@@ -0,0 +1,36 @@
+
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+function get_rvm_gemset {
+  ~/.rvm/bin/rvm-prompt v g
+}
+
+function proml {
+  local        BLUE="\[\033[0;34m\]"
+  local         RED="\[\033[0;31m\]"
+  local   LIGHT_RED="\[\033[1;31m\]"
+  local       GREEN="\[\033[0;32m\]"
+  local LIGHT_GREEN="\[\033[1;32m\]"
+  local       WHITE="\[\033[1;37m\]"
+  local  LIGHT_GRAY="\[\033[0;37m\]"
+  case $TERM in
+    xterm*)
+    TITLEBAR='\[\033]0;\u@\h:\w\007\]'
+    ;;
+    *)
+    TITLEBAR=""
+    ;;
+  esac
+
+PS1="${TITLEBAR}\
+$BLUE[$RED\$(date +%H:%M)$BLUE][$RED\u@\h:\w$BLUE]\n\
+$GREEN[$LIGHT_GRAY\$(get_rvm_gemset)$GREEN]\$(parse_git_branch)\
+$GREEN\$ "
+PS2='> '
+PS4='+ '
+}
+proml
+
+
